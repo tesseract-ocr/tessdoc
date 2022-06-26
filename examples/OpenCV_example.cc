616b29af3e4416532ba12bd5b8213229c177f7eb
zdenop <zdenop@gmail.com>
2022-06-26 16:28:39 +0200

    Update OpenCV_example.cc

--- examples/OpenCV_example.cc
+++ examples/OpenCV_example.cc
@@ -1,17 +1,19 @@
-#include <string>
-#include <tesseract/baseapi.h>
 #include <leptonica/allheaders.h>
 #include <opencv2/opencv.hpp>
-using namespace std;
-using namespace cv;
+#include <string>
+#include <tesseract/baseapi.h>
 
-string outText, imPath = argv[1];
-Mat im = cv::imread(imPath, IMREAD_COLOR);
-tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+int main(int argc, char *argv[]) {
 
-api->Init(NULL, "eng", tesseract::OEM_LSTM_ONLY);
-api->SetPageSegMode(tesseract::PSM_AUTO);
-api->SetImage(im.data, im.cols, im.rows, 3, im.step);
-outText = string(api->GetUTF8Text());
-cout << outText;
-api->End();
+  std::string outText, imPath = argv[1];
+  cv::Mat im = cv::imread(imPath, cv::IMREAD_COLOR);
+  tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+
+  api->Init(NULL, "eng", tesseract::OEM_LSTM_ONLY);
+  api->SetPageSegMode(tesseract::PSM_AUTO);
+  api->SetImage(im.data, im.cols, im.rows, 3, im.step);
+  outText = std::string(api->GetUTF8Text());
+  std::cout << outText;
+  api->End();
+  return 0;
+}
