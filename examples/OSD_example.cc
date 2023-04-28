62824b3bf50b8594711ec0fd2a9ff86b5a6c5e05
Amit D <amitdev2222@gmail.com>
2023-04-28 12:08:12 +0300

    OSD_example.cc: Remove wrong code

--- examples/OSD_example.cc
+++ examples/OSD_example.cc
@@ -6,20 +6,15 @@ int main()
     PIX *image = pixRead(inputfile);
     tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
     api->Init(NULL, "osd");
-// Get OSD - old example code
+    
     tesseract::Orientation orientation;
     tesseract::WritingDirection direction;
     tesseract::TextlineOrder order;
     float deskew_angle;
     api->SetPageSegMode(tesseract::PSM_AUTO_OSD);
     api->SetImage(image);
-    api->Recognize(0);
-    tesseract::PageIterator* it =  api->AnalyseLayout();
-    it->Orientation(&orientation, &direction, &order, &deskew_angle);
-    printf("Orientation: %d;\nWritingDirection: %d\nTextlineOrder: %d\n" \
-    "Deskew angle: %.4f\n",
-    orientation, direction, order, deskew_angle);
-//Get OSD - new code
+    
+    // Get OSD
     int orient_deg;
     float orient_conf;
     const char* script_name;
@@ -29,6 +24,7 @@ int main()
     " Script: %s\n Script confidence: %.2f\n",
     orient_deg, orient_conf,
     script_name, script_conf);
+    
     // Destroy used object and release memory
     api->End();
     pixDestroy(&image);
